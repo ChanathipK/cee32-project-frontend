2953676f6e423cca59136d3a68e96a8aa5ad2c62
SaritPasiphol <99teakteak@gmail.com>
2024-04-18 23:20:41 +0700

    change file name

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static("./public"));
 
 require("dotenv").config()
-app.get('/MainGame', (req, res) => {
-    res.sendFile('/Users/saritpasiphol/Desktop/cee32-project-frontend/public/MainGame.html')
+app.get('/maingame', (req, res) => {
+    res.sendFile('/Users/saritpasiphol/Desktop/cee32-project-frontend/public/maingame.html')
   })
 app.listen(process.env.PORT, () => {
     console.log(`Frontend is ready at localhost:${process.env.PORT}`)
