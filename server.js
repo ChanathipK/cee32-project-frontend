45a88c5b0a697e627f158e9fefdd9ccd6a18abc0
SaritPasiphol <99teakteak@gmail.com>
2024-04-18 22:53:06 +0700

    Add movement and draw

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 app.use(express.static("./public"));
 
 require("dotenv").config()
-
+app.get('/MainGame', (req, res) => {
+    res.sendFile('/Users/saritpasiphol/Desktop/cee32-project-frontend/public/MainGame.html')
+  })
 app.listen(process.env.PORT, () => {
     console.log(`Frontend is ready at localhost:${process.env.PORT}`)
 })
