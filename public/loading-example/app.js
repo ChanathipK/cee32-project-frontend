44055f8f6730b68664a301dd7345f5d5783507dc
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2024-04-20 19:00:07 +0700

    add explanations

--- public/loading-example/app.js
+++ public/loading-example/app.js
@@ -1,12 +1,18 @@
+// get all the needed element
 const loadButton = document.getElementById("load-button");
 const loadingScreen = document.getElementById("loading-screen");
 const loadingMessage = document.getElementById("loading-message");
 const main = document.getElementById("main");
 
+// add event to button when click
 loadButton.addEventListener("click", async () => {
+
+    // as soon as we click load-button, we change some style to loading state
     main.className = "blur";
     loadingScreen.className = "show-flex";
     loadButton.disabled = true;
+
+    // this part is to make the "." in "Loading..." looks better
     let dotCount = 1;
     const tempInterval = setInterval(() => {
         dotCount++;
@@ -19,10 +25,13 @@ loadButton.addEventListener("click", async () => {
         }
         loadingMessage.innerText = message;
     }, 500);
+
+    // mock wait (fake wait for 5 seconds)
     await new Promise((resolve) => {
         setTimeout(resolve, 5000);
     });
 
+    // after waiting finished, we clear interval an set styles back to normal
     clearInterval(tempInterval);
 
     main.className = "";
